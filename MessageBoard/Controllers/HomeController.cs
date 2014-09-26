f3ca6f95cdf7733d7253ee9c6f949674dc6e4e23
Justin Kelly <justinhobbskelly@gmail.com>
2014-09-26 16:32:18 -0500

    Initial commit

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageBoard.Data;
+using MessageBoard.Models;
+using MessageBoard.Services;
+
+namespace MessageBoard.Controllers
+{
+  public class HomeController : Controller
+  {
+    private IMailService _mail;
+    private IMessageBoardRepository _repo;
+
+      public HomeController(IMailService mail, IMessageBoardRepository repo)
+    {
+        _repo = repo;
+        _mail = mail;
+    }
+    public ActionResult Index()
+    {
+      ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+        var topics = _repo.GetTopics().OrderByDescending(p => p.Created).Take(25).ToList();
+      return View(topics);
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your app description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Contact(ContactModel model)
+    {
+        var message = string.Format("Comment From: {1}{0}Email: {2}{0}Website: {3}{0}Comment: {4}{0}", 
+            Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+        var svc = new MailService();
+        if(_mail.SendMail("justin@babalan.com", "noreply@babalan.com", "Message from site", message))
+        {
+            ViewBag.MailSent = true;
+        }
+        return View();
+    }
+      [Authorize]
+    public ActionResult MyMessages()
+    {
+        return View();
+    }
+
+  }
+}
---
+++ MessageBoard/Data/MessageBoardMigrationsConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+
+namespace MessageBoard.Data
+{
+    public class MessageBoardMigrationsConfiguration:DbMigrationsConfiguration<MessageBoardContext>
+    {
+        public MessageBoardMigrationsConfiguration()
+        {
+            this.AutomaticMigrationDataLossAllowed = true;
+            this.AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MessageBoardContext context)
+        {
+
+            
+#if DEBUG
+            if (context.Topics.Count() == 0)
+            {
+                var topic = new Topic()
+                {
+                    Title = "Really interesting subject matter",
+                    Body = "Seriously this is the coolest thing ever",
+                    Created = DateTime.Now,
+                    Replies = new List<Reply>()
+                    {
+                        new Reply()
+                        {
+                            Body = "very interesting indeed!",
+                            Created = DateTime.Now
+                        },
+                        new Reply()
+                        {
+                            Body = "eh, it's ok",
+                            Created = DateTime.Now
+                        },
+                        new Reply()
+                        {
+                            Body = "mildly interesting...",
+                            Created = DateTime.Now
+                        }
+                    }
+
+                };
+                context.Topics.Add(topic);
+                var topic2 = new Topic()
+                {
+                    Title = "An additional topic that is very interesting",
+                    Body = "This one is better than the last one",
+                    Created = DateTime.Now,
+                    Replies = new List<Reply>()
+                    {
+                        new Reply()
+                        {
+                            Body = "this one is even better!",
+                            Created = DateTime.Now
+                        }
+                    }
+
+                };
+                context.Topics.Add(topic2);
+                context.SaveChanges();
+            }
+            
+#endif
+        }
+    }
+}
